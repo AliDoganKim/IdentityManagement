90176f790b71640f314b1b14c2f36333a5335f30
Ali DOĞAN <alidogans@outlook.com>
2020-05-30 23:58:06 +0300

    .

---
+++ IdentityManagement.Server/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using IdentityManagement.Infrastructure.Persistence;
+using IdentityManagement.Server.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityManagement.Server.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost("/api/user/register")]
+        public async Task<IActionResult> Register([FromBody]RegisterRequestViewModel request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var user = new AppUser { UserName = request.Email, Name = request.Name, Email = request.Email };
+            var result = await _userManager.CreateAsync(user, request.Password);
+            if (!result.Succeeded)
+                return BadRequest(result.Errors);
+
+            await _userManager.AddClaimAsync(user, new Claim("userName", user.UserName));
+            await _userManager.AddClaimAsync(user, new Claim("name", user.Name));
+            await _userManager.AddClaimAsync(user, new Claim("email", user.Email));
+            await _userManager.AddClaimAsync(user, new Claim("role", "user"));
+            return Ok();
+        }
+    }
+}
