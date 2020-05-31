5bbe4308bb1af81343103382b4bd6470af94862b
Ali DOĞAN <alidogans@outlook.com>
2020-05-31 09:17:32 +0300

    .

--- IdentityManagement.Server/Startup.cs
+++ IdentityManagement.Server/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityManagement.Infrastructure.Extensions;
+using IdentityManagement.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
