da7150b2bed0e400e76fe73131f57d2350ba3187
Ali DOĞAN <alidogans@outlook.com>
2020-06-13 10:59:56 +0300

    web login izin ayarları ve geliştirme modunda anasayfanın gelmesi

---
+++ IdentityManagement.Server/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityManagement.Server.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _environment;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(IWebHostEnvironment environment, ILogger<HomeController> logger)
+        {
+            _environment = environment;
+            _logger = logger;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            if (_environment.IsDevelopment())
+            {
+                // only show in development
+                return View();
+            }
+
+            _logger.LogInformation("Homepage is disabled in production. Returning 404.");
+            return NotFound();
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Privacy()
+        {
+            var claims = HttpContext.User.Claims.Select(x => $"{x.Type}:{x.Value}");
+            return Ok(new
+            {
+                Name = "Values API",
+                Claims = claims.ToArray()
+            });
+            //return View();
+        }
+    }
+}
